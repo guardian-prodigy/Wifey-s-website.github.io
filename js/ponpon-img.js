a573e8746df31904e281649abf2a30de763f73a8
Guardian Prodigy <devilmusicmutant@gmail.com>
2021-05-13 11:21:24 -0300

    added stuff

--- js/ponpon-img.js
+++ js/ponpon-img.js
@@ -1,18 +1,22 @@
-function Button() {
+function ButtonForShowMorePon() {
     let paragraph = document.getElementById
     ('more-images');
     let button = 1;
-    let button1 = document.querySelector('#button-show-more-img');
+    let button1 = document.getElementById('button-show-more-img');
+    button1.style.background = "linear-gradient(45deg, black, red)";
     button1.addEventListener('click', function(e) {
+        let button1 = document.getElementById('button-show-more-img');
         if (e.target == button1 && button == 1) {
             paragraph.style.display = "inherit";
+            button1.style.background = "linear-gradient(45deg, black, green)";
             button = 0;
             button1.textContent = "Show Less"
         } else if ( e.target == button1 && button == 0) {
             paragraph.style.display = "none";
+            button1.style.background = "linear-gradient(45deg, black, red)"
             button = 1;
             button1.textContent = "Show More Pon"
         }
     })
 }
-Button()
+ButtonForShowMorePon()

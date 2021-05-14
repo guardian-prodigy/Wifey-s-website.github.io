f0bd5064445ca46bb09c28c5fa7fedcd16eeb7a4
Guardian Prodigy <devilmusicmutant@gmail.com>
2021-05-14 19:07:35 -0300

    still haave toi add close button animation

--- js/ponpon-img.js
+++ js/ponpon-img.js
@@ -4,17 +4,18 @@ function ButtonForShowMorePon() {
     let button = 1;
     let button1 = document.getElementById('button-show-more-img');
     button1.style.background = "linear-gradient(45deg, black, red)";
+    
     button1.addEventListener('click', function(e) {
         if (e.target == button1 && button == 1) {
             paragraph.style.display = "inherit";
             button1.style.background = "linear-gradient(45deg, black, green)";
             button = 0;
-            button1.textContent = "Show Less"
+            button1.textContent = "Show Less";
         } else if ( e.target == button1 && button == 0) {
             paragraph.style.display = "none";
             button1.style.background = "linear-gradient(45deg, black, red)"
             button = 1;
-            button1.textContent = "Show More Pon"
+            button1.textContent = "Show More Pon";
         }
     })
 }

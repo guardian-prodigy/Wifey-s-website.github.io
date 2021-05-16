7bc6ee34603b0b164972a79bd312664aca96860e
Guardian Prodigy <devilmusicmutant@gmail.com>
2021-05-16 18:23:28 -0300

    added button animation almost good

--- js/ponpon-img.js
+++ js/ponpon-img.js
@@ -1,20 +1,19 @@
 function ButtonForShowMorePon() {
     let paragraph = document.querySelector('.more-images');
     let button = 1
-    let button1 = document.getElementById('button-show-more-img');
-    button1.style.background = "linear-gradient(45deg, black, red)";
+    let button1 = document.querySelector('.button-show-more-img');
     button1.addEventListener('click', function () {
         paragraph.classList.toggle('open');
     })
     button1.addEventListener('click', (e) => {
+        
         if (e.target == button1 && button == 1) {
             button1.textContent = "Show Less";
             button = 0;
-            button1.style.background = "linear-gradient(45deg, black, green, black)"
+            
         } else if (e.target == button1 && button == 0) {
             button1.textContent = "Show More Pon";
             button = 1;
-            button1.style.background = "linear-gradient(45deg, black, red, black)"
         }
     })
 }

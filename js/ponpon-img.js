f0502e406995b5f8a2921c6833d05eaf32bcab4f
guardian-prodigy <79934767+guardian-prodigy@users.noreply.github.com>
2021-05-19 20:03:57 -0300

    added headpands html without divs

--- js/ponpon-img.js
+++ js/ponpon-img.js
@@ -18,7 +18,6 @@ function ButtonForShowMorePon() {
     })
 }
 ButtonForShowMorePon()
-
 function ButtontoDisplayPrices() {
     let paragraph = document.querySelector('.paragraph-for-prices');
     let button1 = document.querySelector(".button-to-display-prices");
@@ -27,12 +26,12 @@ function ButtontoDisplayPrices() {
         if (e.target == button1 && button == 1){
            paragraph.classList.toggle('open1');
            paragraph.classList.remove('open2');
-           button = 0 
+           button = 0;
            button1.textContent = "Close Prices";
         } else if (e.target == button1 && button == 0) {
             paragraph.classList.remove('open1');
             paragraph.classList.toggle('open2');
-            button = 1
+            button = 1;
             button1.textContent = "Display Prices";
         }
         

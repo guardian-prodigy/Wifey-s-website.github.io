2aba61f8a3237e4086fbd8af5e9f5a78f08e1716
guardian-prodigy <79934767+guardian-prodigy@users.noreply.github.com>
2021-05-19 15:10:52 -0300

    added better java

--- js/ponpon-img.js
+++ js/ponpon-img.js
@@ -25,11 +25,13 @@ function ButtontoDisplayPrices() {
     let button = 1
     button1.addEventListener('click', (e) => {
         if (e.target == button1 && button == 1){
-           paragraph.classList.toggle('open');
+           paragraph.classList.toggle('open1');
+           paragraph.classList.remove('open2');
            button = 0 
            button1.textContent = "Close Prices";
         } else if (e.target == button1 && button == 0) {
-            paragraph.classList.remove('open');
+            paragraph.classList.remove('open1');
+            paragraph.classList.toggle('open2');
             button = 1
             button1.textContent = "Display Prices";
         }
